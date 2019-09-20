bd9415e221338c6fc9439e3fb96716ca9227be11
Nate Peterson <ntpeters@mtu.edu>
2019-09-20 15:58:03 -0700

    Fix util module loading on Windows

--- config/powershell/ntpetersUtil.psm1
+++ config/powershell/ntpetersUtil.psm1
@@ -5,16 +5,17 @@ $scriptDirectory = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 . $scriptDirectory\functions\Export-Variable.ps1
 . $scriptDirectory\functions\Invoke-Exa.ps1
 . $scriptDirectory\functions\Show-Colors.ps1
-. $scriptDirectory\functions\Test-Administrator.ps1
 
-Export-ModuleMember -Alias export, exa, colors, isadmin -Function 'Export-Variable', 'Invoke-Exa', 'Show-Colors', 'Test-Administrator'
+Export-ModuleMember -Alias export, exa, colors -Function 'Export-Variable', 'Invoke-Exa', 'Show-Colors'
 
 # Load Windows only functions.
 # We're running on Windows if either:
 #   - $IsWindows is true (PowerShell Core)
 #   - $IsWindows does not exist (Windows PowerShell)
-If ($Script:IsWindows -Eq $False) {
+$Script:IsWindows = Get-Variable 'IsWindows' -Scope 'Global' -ErrorAction 'Ignore'
+If ($Script:IsWindows -Eq $True) {
     . $scriptDirectory\functions\Invoke-XLaunch.ps1
+    . $scriptDirectory\functions\Test-Administrator.ps1
 
-    Export-ModuleMember -Alias startx -Function 'Invoke-XLaunch'
+    Export-ModuleMember -Alias startx, isadmin -Function 'Invoke-XLaunch', 'Test-Administrator'
 }
