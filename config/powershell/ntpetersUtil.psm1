5e70e6dc9f38ef250ba41a7cadbc4a19a14311e1
Nate Peterson <ntpeters@mtu.edu>
2020-03-19 16:24:04 -0700

    Update scoping for util module

--- config/powershell/ntpetersUtil.psm1
+++ config/powershell/ntpetersUtil.psm1
@@ -1,21 +1,20 @@
-if (Get-Module ntpetersUtil) { return }
+# Only import the module once.
+If (Get-Module ntpetersUtil) {
+    return
+}
 
-$scriptDirectory = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+$Script:ScriptDirectory = (Split-Path -Parent $MyInvocation.MyCommand.Definition)
 
-. $scriptDirectory\functions\Export-Variable.ps1
-. $scriptDirectory\functions\Invoke-Exa.ps1
-. $scriptDirectory\functions\Show-Colors.ps1
+. $Script:ScriptDirectory\functions\Export-Variable.ps1
+. $Script:ScriptDirectory\functions\Invoke-Exa.ps1
+. $Script:ScriptDirectory\functions\Show-Colors.ps1
 
 Export-ModuleMember -Alias export, exa, colors -Function 'Export-Variable', 'Invoke-Exa', 'Show-Colors'
 
 # Load Windows only functions.
-# We're running on Windows if either:
-#   - $IsWindows is true (PowerShell Core)
-#   - $IsWindows does not exist (Windows PowerShell)
-$Script:IsOsWindows = Get-Variable 'IsWindows' -Scope 'Global' -ErrorAction 'Ignore'
-If ($Script:IsOsWindows -Eq $True) {
-    . $scriptDirectory\functions\Invoke-XLaunch.ps1
-    . $scriptDirectory\functions\Test-Administrator.ps1
+If ($Global:IsWindows) {
+    . $Script:ScriptDirectory\functions\Invoke-XLaunch.ps1
+    . $Script:ScriptDirectory\functions\Test-Administrator.ps1
 
     Export-ModuleMember -Alias startx, isadmin -Function 'Invoke-XLaunch', 'Test-Administrator'
 }
