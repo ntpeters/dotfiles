436b17d7ccdab80774297597d0b4da76c813674d
Nate Peterson <ntpeters@mtu.edu>
2020-03-19 14:00:27 -0700

    Improve PowerShell environment script

--- config/powershell/environment.ps1
+++ config/powershell/environment.ps1
@@ -1,6 +1,16 @@
 # Sets environment variables for the current PowerShell instance,
 # and for the current user if running on Windows.
 
+# Replicate the `IsWindows` environment variable from PowerShell Core in Windows PowerShell too.
+# This is intentionally placed directly in the `Global` scope rather than the `Env` scope to match PowerShell Core.
+# We're running on Windows if either:
+#   - $IsWindows is true (PowerShell Core)
+#   - $IsWindows does not exist (Windows PowerShell)
+If ($Null -Eq (Get-Variable 'IsWindows' -Scope 'Global' -ErrorAction 'Ignore')) {
+    New-Variable 'IsWindows' -Value $True -Scope 'Global' -Option 'ReadOnly'
+}
+
+# Ensure utilities are imported so that `Export-Variable` is defined.
 $Util = "${Env:UserProfile}\.config\powershell\ntpetersUtil.psm1"
 If (Test-Path($Util)) {
     Import-Module "$Util"
@@ -26,8 +36,6 @@ If ($Null -Ne $(Get-Command fd -ErrorAction 'Ignore')) {
     Export-Variable 'FZF_DEFAULT_COMMAND' "fd --type file"
 }
 
-# Source local PowerShell environment if one exists.
-$LocalPowerShellEnv = "${Env:UserProfile}\.config\powershell\local_environment.ps1"
-If (Test-Path $LocalPowerShellEnv) {
-    . $LocalPowerShellEnv
-}
+# Counterpart to the existing $Profile variable pointing to a local profile not synced with dotfiles.
+# This is intentionally placed directly in the `Global` scope rather than the `Env` scope to match $Profile.
+$Global:LocalProfile = "${Env:UserProfile}\.config\powershell\local_profile.ps1"
