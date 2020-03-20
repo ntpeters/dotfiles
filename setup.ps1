3ad7b72501be84960f89d3d3e6840b8f625e4b31
Nate Peterson <ntpeters@mtu.edu>
2020-03-19 17:00:29 -0700

    Update PowerShell setup script

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,17 @@
 # Setup script for Windows
 
+# Load Windows only functions.
 # We're running on Windows if either:
 #   - $IsWindows is true (PowerShell Core)
 #   - $IsWindows does not exist (Windows PowerShell)
 $Script:IsOsWindows = Get-Variable 'IsWindows' -Scope 'Global' -ErrorAction 'Ignore'
 If ($Script:IsOsWindows -Eq $False) {
     Throw 'This script is intended for Windows only'
+}
+
+# Honor user-specified ErrorAction if one was given, otherwise stop on error
+If (-Not ($MyInvocation.BoundParameters.ContainsKey('ErrorAction'))) {
+    $ErrorActionPreference = "Stop"
 }
 
 # Create PowerShell directory if it doesn't already exist
@@ -16,23 +22,19 @@ If (-Not (Test-Path $Script:WindowsPowerShellHome)) {
 
 # Use the same directory for Windows PowerShell and PowerShell Core
 $Script:PowerShellCoreHome = "${Env:UserProfile}\Documents\PowerShell"
-If (-Not (Test-Path $Script:PowerShellCoreHome)) {
-    New-Item -Path $Script:PowerShellCoreHome -ItemType Junction -Value "${Env:UserProfile}\Documents\PowerShell"
-}
+New-Link -TargetPath $Script:WindowsPowerShellHome -LinkPath $Script:PowerShellCoreHome -LinkType 'Junction'
 
 # If the user's documents directory is not at '~/Documents', link the PowerShell directories into place there too.
 # This will be the case if 'Documents' is being backed up to OneDrive.
 $Script:MyDocumentsPath = [Environment]::GetFolderPath("MyDocuments")
 If ($Script:MyDocumentsPath -Ne "${Env:UserProfile"}\Documents") {
-    New-Item -Path $Script:WindowsPowerShellHome -ItemType Junction -Value "${Script:MyDocumentsPath}\WindowsPowerShell"
-    New-Item -Path $Script:PowerShellCoreHome -ItemType Junction -Value "${Script:MyDocumentsPath}\PowerShell"
+    New-Link -TargetPath $Script:WindowsPowerShellHome -LinkPath "${Script:MyDocumentsPath}\WindowsPowerShell" -LinkType 'Junction'
+    New-Link -TargetPath $Script:PowerShellCoreHome -LinkPath "${Script:MyDocumentsPath}\PowerShell" -LinkType 'Junction'
 }
 
 # Link PowerShell profile into place
 $Script:PowerShellProfile = "${Script:WindowsPowerShellHome}\Microsoft.PowerShell_profile.ps1"
-If (-Not (Test-Path $Script:PowerShellProfile)) {
-    New-Item -Path $Script:PowerShellProfile -ItemType 'SymbolicLink' -Value "${Env:UserProfile}\.config\powershell\profile.ps1"
-}
+New-Link -TargetPath "${Env:UserProfile}\.config\powershell\profile.ps1" -LinkPath $Script:PowerShellProfile -LinkType 'SymbolicLink'
 
 # Source the PowerShell profile
 . $Script:PowerShellProfile
@@ -44,14 +46,10 @@ If ($Null -Ne $(Get-Command concfg -ErrorAction 'Ignore')) {
 }
 
 # Link NeoVim config into place
-$Script:NeoVimConfigDir= "${Env:UserProfile}\AppData\Local\nvim"
+$Script:NeoVimConfigDir= "${Env:LocalAppData}\nvim"
 $Script:NeoVimConfig = "${Script:NeoVimConfigDir}\init.vim"
-If (-Not (Test-Path $Script:NeoVimConfig)) {
-    New-Item -Path $Script:NeoVimConfig -ItemType 'SymbolicLink' -Value "${Env:UserProfile}\.config\nvim\init.vim"
-}
+New-Link -TargetPath "${Env:UserProfile}\.config\nvim\init.vim" -LinkPath $Script:NeoVimConfig -LinkType 'SymbolicLink'
 
 # Link NeoVim GUI config into place
-$Script:NeoVimGuiConfig = "${$Script:NeoVimConfigDir}\ginit.vim"
-If (-Not (Test-Path $Script:NeoVimGuiConfig)) {
-    New-Item -Path $Script:NeoVimGuiConfig -ItemType 'SymbolicLink' -Value "${Env:UserProfile}\.config\nvim\ginit.vim"
-}
+$Script:NeoVimGuiConfig = "${Script:NeoVimConfigDir}\ginit.vim"
+New-Link -TargetPath "${Env:UserProfile}\.config\nvim\ginit.vim" -LinkPath $Script:NeoVimGuiConfig -LinkType 'SymbolicLink'
