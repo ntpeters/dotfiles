90f8f4a0cccb7fc5dd5ce16fc2eab5c4a90e7f48
Nate Peterson <ntpeters@mtu.edu>
2022-03-24 14:29:37 -0700

    Update Export-Variable command

--- config/powershell/functions/Export-Variable.ps1
+++ config/powershell/functions/Export-Variable.ps1
@@ -5,12 +5,16 @@ Function Export-Variable {
     If the value is omitted, the name is parsed as an expression of the form "Name=Value",
     and if successful the resulting name/value pair is used to set the environment variable.
     #>
+    [CmdletBinding(SupportsShouldProcess)]
     Param (
         [Parameter(Position=0)]
         [string]$Name,
 
         [Parameter(Position=1)]
-        [string]$Value
+        [string]$Value,
+
+        [switch]
+        $Force
     )
 
     If ([string]::IsNullOrEmpty($Name)) {
@@ -28,17 +32,53 @@ Function Export-Variable {
     # These checks are needed due to `SetEnvironmentVariable` automatically broadcasting a `WM_SETTINGCHANGE` message
     # and waiting for all top-level windows to respond.
 
+    $userEnvironmentTarget = [System.EnvironmentVariableTarget]::User
+    $processEnvironmentTarget = [System.EnvironmentVariableTarget]::Process
+    $userEnvironmentTargetString = $userEnvironmentTarget.ToString().ToLower()
+    $processEnvironmentTargetString = $processEnvironmentTarget.ToString().ToLower()
+    $commonMessageFragmentFormat = "variable '$Name' for the current {0} with value of: '$Value'"
+    $commonNotProcessMessageFragmentFormat = "export as the current value is equal to the provided value for $commonMessageFragmentFormat"
+
     # Set the environment variable for the current user.
-    $CurrentUserValue = [Environment]::GetEnvironmentVariable($Name, [System.EnvironmentVariableTarget]::User)
-    If ($CurrentUserValue -Ne $Value) {
-        [Environment]::SetEnvironmentVariable($Name, $Value, [System.EnvironmentVariableTarget]::User)
+    $shouldSetUserVariable = $Force -or ([Environment]::GetEnvironmentVariable($Name, $userEnvironmentTarget) -Ne $Value)
+    if ($shouldSetUserVariable) {
+        $shouldProcessCurrentUserMessage = "Exporting $commonMessageFragmentFormat" -f $userEnvironmentTargetString
+        $verboseCurrentUserMessage = "Exported $commonMessageFragmentFormat" -f $userEnvironmentTargetString
+    } else {
+        $shouldProcessCurrentUserMessage = "Skipping $commonNotProcessMessageFragmentFormat" -f $userEnvironmentTargetString
+        $verboseCurrentUserMessage = "Skipped $commonNotProcessMessageFragmentFormat" -f $userEnvironmentTargetString
+    }
+    if ($PSCmdlet.ShouldProcess($shouldProcessCurrentUserMessage, $Name ,"Export-Variable")) {
+        if ($shouldSetUserVariable) {
+            [Environment]::SetEnvironmentVariable($Name, $Value, $userEnvironmentTarget)
+        }
+        Write-Verbose $verboseCurrentUserMessage
     }
 
     # Also make the variable available immediately in the current session.
-    $CurrentProcessValue = [Environment]::GetEnvironmentVariable($Name, [System.EnvironmentVariableTarget]::Process)
-    If ($CurrentProcessValue -Ne $Value) {
-        [Environment]::SetEnvironmentVariable($Name, $Value, [System.EnvironmentVariableTarget]::Process)
+    $shouldSetProcessVariable = $Force -or ([Environment]::GetEnvironmentVariable($Name, $processEnvironmentTarget) -Ne $Value)
+    if ($shouldSetProcessVariable) {
+        $shouldProcessCurrentProcessMessage = "Exporting $commonMessageFragmentFormat" -f $processEnvironmentTargetString
+        $verboseCurrentProcessMessage = "Exported $commonMessageFragmentFormat" -f $processEnvironmentTargetString
+    } else {
+        $shouldProcessCurrentProcessMessage = "Skipping $commonNotProcessMessageFragmentFormat" -f $processEnvironmentTargetString
+        $verboseCurrentProcessMessage = "Skipped $commonNotProcessMessageFragmentFormat" -f $processEnvironmentTargetString
+    }
+    if ($PSCmdlet.ShouldProcess($shouldProcessCurrentProcessMessage, $Name ,"Export-Variable")) {
+        if ($shouldSetProcessVariable) {
+            [Environment]::SetEnvironmentVariable($Name, $Value, $processEnvironmentTarget)
+        }
+        Write-Verbose $verboseCurrentProcessMessage
     }
 }
 
 Set-Alias export Export-Variable
+
+# TODO: add function to refresh environment variables
+# adapt the one from choco: https://github.com/chocolatey/choco/blob/stable/src/chocolatey.resources/helpers/functions/Update-SessionEnvironment.ps1
+
+# TODO: fix handling of expandable values on Windows
+# relevant dotnet bug: https://github.com/dotnet/runtime/issues/1442
+# choco workarounds as references:
+# https://github.com/chocolatey/choco/blob/stable/src/chocolatey.resources/helpers/functions/Get-EnvironmentVariable.ps1
+# https://github.com/chocolatey/choco/blob/stable/src/chocolatey.resources/helpers/functions/Set-EnvironmentVariable.ps1
