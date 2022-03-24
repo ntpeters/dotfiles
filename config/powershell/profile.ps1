01c2847d29f0dc986cf31627aff598f7e407c0b1
Nate Peterson <ntpeters@mtu.edu>
2022-03-24 10:50:05 -0700

    Add PSFzf module to setup script

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -1,13 +1,19 @@
 # PowerShell profile
 
 # Set encoding
-$OutputEncoding = [Text.Encoding]::UTF8
+# Specifically not using Text.Encoding.UTF8 here, since that encoding includes a BOM which breaks terminal apps (especially when piping).
+# This is the same encoding set by default for PowerShell Core, but it's set explicitly here for somewhat consistent behavior in Windows PowerShell (except for piping, which always seems to have a BOM added regardless of encoding).
+$OutputEncoding = [Text.UTF8Encoding]::new()
+$InputEncoding = [Text.UTF8Encoding]::new()
 [Console]::OutputEncoding = $OutputEncoding
+[Console]::InputEncoding = $InputEncoding
 $PSDefaultParameterValues["Out-File:Encoding"] = "UTF8"
 CHCP 65001 | Out-Null
 
 # Init pshazz if available and not already initialized
-If ((-Not $pshazz) -And ($Null -Ne $(Get-Command pshazz -ErrorAction 'Ignore'))) {
+if ($null -eq $(Get-Command pshazz -ErrorAction 'Ignore')) {
+    # TODO: set a default prompt
+} elseif (-not $pshazz) {
     pshazz init
 }
 
@@ -18,9 +24,14 @@ If ((-Not $pshazz) -And ($Null -Ne $(Get-Command pshazz -ErrorAction 'Ignore')))
 . "${Env:UserProfile}\.config\powershell\environment.ps1"
 
 # Aliases
-Invoke-Expression "$(thefuck --alias)"
-Set-Alias fu fuck
-Set-Alias pe path-extractor
+if ($null -ne $(Get-Command thefuck -ErrorAction 'Ignore')) {
+    Invoke-Expression "$(thefuck --alias)"
+    Set-Alias fu fuck
+}
+
+if ($null -ne $(Get-Command path-extractor -ErrorAction 'Ignore')) {
+    Set-Alias pe path-extractor
+}
 
 # PSReadLine Settings
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
@@ -31,6 +42,14 @@ Set-PSReadlineKeyHandler "Ctrl+Backspace" BackwardKillWord
 Set-PSReadlineKeyHandler "Ctrl+LeftArrow" BackwardWord
 Set-PSReadlineKeyHandler "Ctrl+RightArrow" NextWord
 Set-PSReadlineKeyHandler "Tab" MenuComplete
+
+if (($null -ne (Get-Module -Name PSFzf -ListAvailable -ErrorAction Ignore)) -and ($null -ne (Get-Command fzf -ErrorAction Ignore))) {
+    # Import-Module PSFzf
+
+    # Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+    Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r' -EnableFd -FileSystemCommand 'fd . --color always --type directory {0}'
+    # -FileSystemCommandDefaultArgs '--type file' -FileSystemCommandDirectoryOnlyArgs '--type directory' -FileSystemCommandColorArgs '--color always'
+}
 
 # Import custom functions
 $Util = "${Env:UserProfile}\.config\powershell\ntpetersUtil.psm1"
